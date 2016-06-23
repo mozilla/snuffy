552a3cbea63f2934ee10a1a9ac339b7f0fca246e
Stephan Leroux <sleroux@mozilla.com>
2016-06-23 15:57:22 -0400

    Fix whitespacing in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: ['babel-polyfill', './index.js'],
-  output: {
-    path: './bin',
-    filename: 'fathom.bundle.js',
-    libraryTarget: "var",
-    library: "fathom"
-  },
-  module: {
-    loaders: [{
-      text: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  }
+    entry: ['babel-polyfill', './index.js'],
+    output: {
+        path: './bin',
+        filename: 'fathom.bundle.js',
+        libraryTarget: "var",
+        library: "fathom"
+    },
+    module: {
+        loaders: [{
+            text: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
 };
