7eb3db05df5811e6121f3c9741026d4ba9c36aec
Swathi Iyer <swathii@stanford.edu>
2017-07-11 15:47:23 -0700

    product image and title

--- examples/products.js
+++ examples/products.js
@@ -1,19 +1,43 @@
+/**
+ * An algorithm that extracts a product from a product page,
+ * where a 'product' is defined by the bundle of features that
+ * makes it identifiable.
+ *
+ * Features:
+ *    Implemented/In Progress - Title, Image
+ *    Near Future -  Price
+ *    Farther Future - Reviews, Product ID?
+ *
+ * Testing:
+ *    10 test products in product_classification_test_data folder
+ *    See output in Karma DEBUG RUNNER at http://localhost:9876/debug.html JS console
+ *    All test sources are product detail pages (one product on this page)
+ *    Later, go on to extract products from a product index page (multiple products per page)
+ *
+ * Other:
+ *    Identifying detail vs index page - classification problem?
+ */
+
 var fs = require('fs');
-const {dirname, join} = require('path');
-
+const {dirname} = require('path');
 const leven = require('leven');
-
 const {dom, props, out, rule, ruleset, score, type} = require('../index');
-const {domSort, inlineTextLength, linkDensity, staticDom} = require('../utils');
+const {domSort, staticDom} = require('../utils');
 const {Annealer} = require('../optimizers');
 
 
+
 function euclideanDistance(nodeA, nodeB){
+
+  /* getBoundingClientRect is relative to the viewport, should be fine for tests and demos
+  but might need to be tweaked or replaced with a different call when integrated
+  into a real-life browsing session, where the user might have scrolled */
+
   var rectA = nodeA.getBoundingClientRect();
   var rectB = nodeB.getBoundingClientRect();
   var x = rectB.left - rectA.left;
   var y = rectB.bottom - rectB.bottom;
-  return Math.sqrt(x^2 + y^2);
+  return Math.sqrt(x**2 + y**2);
 }
 
 function tunedImageFnodes() {
@@ -21,27 +45,32 @@ function tunedImageFnodes() {
       return fnode.element.offsetWidth * fnode.element.offsetHeight;
     }
 
-    function imageHasSrc(fnode){
-      return fnode.element.hasAttribute('src') && fnode.element.getAttribute('src') != '';
+    function imageHasSrc(fnode) {
+      return fnode.element.hasAttribute('src') && fnode.element.getAttribute('src') !== '';
     }
 
-    function imageTitle(fnode){
+    function imageTitle(fnode) {
       var title = document.getElementsByTagName('title')[1];
-      if(title === undefined){
-        return 0;
+      if (title === undefined) {
+        return 1;
       }
 
-      // return fnode.element.getAttribute('title') &&
-      //        (fnode.element.getAttribute('title').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('title'))) ||
-      //        fnode.element.getAttribute('alt') &&
-      //        (fnode.element.getAttribute('alt').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('alt')));
       if (fnode.element.getAttribute('title') &&
-             (fnode.element.getAttribute('title').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('title'))) ||
-             fnode.element.getAttribute('alt') &&
-             (fnode.element.getAttribute('alt').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('alt')))){
+         (fnode.element.getAttribute('title').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('title'))) ||
+          fnode.element.getAttribute('alt') &&
+         (fnode.element.getAttribute('alt').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('alt')))) {
                return 10000;
-             };
+      }
+      return 1;
     }
+
+    function thumbnail(fnode) {
+      if(fnode.element.hasAttribute('src') && fnode.element.getAttribute('src').includes('thumb')){
+        return 0;
+      }
+      return 1;
+    }
+
 
     const rules = ruleset(
       //get all images
@@ -56,6 +85,9 @@ function tunedImageFnodes() {
       //image title matches page title
       rule(type('images'), score(imageTitle)),
 
+      //not to be confused with the thumbnail
+      rule(type('images'), score(thumbnail)),
+
       //return image with max score
       rule(type('images').max(), out('main-image'))
 
@@ -68,28 +100,60 @@ function tunedImageFnodes() {
     return contentFnodes;
 }
 
+function tunedTitleFnodes() {
+    const rules = ruleset(
+      //get all title tags in the inserted fixture
+      rule(dom('div#fixture > title'), type('titleish')),
+
+      //return image with max score
+      rule(type('titleish').max(), out('product-title'))
+
+    );
+
+    function contentFnodes(doc) {
+        return rules.against(doc).get('product-title');
+    }
+
+    return contentFnodes;
+}
+
 /**
  * Maintain state as we compare a series of DOMs, reporting the percent
  * difference at the end.
  */
 class DiffStats {
-    constructor(contentFnodes) {
+    constructor(contentFnodes, feature) {
         this.lengthOfExpectedTexts = 0;
         this.lengthOfDiffs = 0;
-        this.contentFnodes = contentFnodes || tunedImageFnodes();
+        if (feature === 'image') {
+          this.contentFnodes = contentFnodes || tunedImageFnodes();
+          this.feature = 'image';
+        } else {
+          this.contentFnodes = contentFnodes || tunedTitleFnodes();
+          this.feature = 'title';
+        }
     }
 
     compare(expectedDom, sourceDom) {
-        //compare images by src
-        const expectedText = expectedDom.body.firstChild.getAttribute('src');
-        const gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.getAttribute('src'))[0];
+        var expectedText;
+        var gotText;
+        if (this.feature === 'image') {
+          //compare images by src
+          expectedText = expectedDom.body.firstChild.getAttribute('src');
+          gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.getAttribute('src'))[0];
+        } else {
+          //compare innerHTML text of titles
+          expectedText = expectedDom.head.firstChild.innerHTML;
+          gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.innerHTML)[0];
+        }
 
-        this.lengthOfExpectedTexts += expectedText.length;
-        this.lengthOfDiffs += leven(expectedText, gotText);
-
-        console.log(leven(expectedText, gotText), expectedText.length, leven(expectedText, gotText)/expectedText.length);
+        this.lengthOfExpectedTexts++;
+        if(expectedText !== gotText) {
+          this.lengthOfDiffs++;
+        }
 
         // Uncomment for debugging:
+        console.log(leven(expectedText, gotText), expectedText.length, leven(expectedText, gotText)/expectedText.length);
         console.log('Got:\n' + gotText);
         console.log('\nExpected:\n' + expectedText);
     }
@@ -100,15 +164,16 @@ class DiffStats {
 }
 
 
-function deviationScore(docPairs, coeffs = []) {
-    const stats = new DiffStats(tunedImageFnodes(...coeffs));
-    window.width = 1661;
-    window.height = 562;
+function deviationScore(docPairs, feature, coeffs = []) {
+    var stats;
+    if(feature === 'image') {
+      stats = new DiffStats(tunedImageFnodes(...coeffs), 'image');
+    } else {
+      stats = new DiffStats(tunedTitleFnodes(...coeffs), 'title');
+    }
+
     for (let pair of docPairs) {
         window.document.body.insertAdjacentHTML('afterbegin', '<div id = "fixture">' + pair[1] + '</div>');
-        if(document.getElementsByTagName('title')[1] !== undefined){
-          console.log(document.getElementsByTagName('title')[1].innerHTML);
-        }
         stats.compare(pair[0], window.document);
         window.document.body.removeChild(document.getElementById('fixture'));
     }
@@ -116,8 +181,7 @@ function deviationScore(docPairs, coeffs = []) {
     return stats.score();
 }
 
-/** Return (expected DOM, source DOM) for all the readbaility test docs. */
-function productDocPairs() {
+function productImageDocPairs() {
   return [
     [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/amazon/expected-image.html', 'utf-8')),
       fs.readFileSync(__dirname + '/../test/product_classification_test_data/amazon/source.html', 'utf-8')
@@ -134,22 +198,70 @@ function productDocPairs() {
      [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/newegg/expected-image.html', 'utf-8')),
       fs.readFileSync(__dirname + '/../test/product_classification_test_data/newegg/source.html', 'utf-8')
     ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/uniqlo/expected-image.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/uniqlo/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/h&m/expected-image.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/h&m/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/barnes&noble/expected-image.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/barnes&noble/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/bose/expected-image.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/bose/source.html', 'utf-8')
+    ],
     [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/target/expected-image.html', 'utf-8')),
       fs.readFileSync(__dirname + '/../test/product_classification_test_data/target/source.html', 'utf-8')
     ]
   ];
 }
+function productTitleDocPairs() {
+  return [
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/amazon/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/amazon/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/craigslist/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/craigslist/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/ebay/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/ebay/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/etsy/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/etsy/source.html', 'utf-8')
+    ],
+     [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/newegg/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/newegg/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/uniqlo/expected-title.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/uniqlo/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/h&m/expected-title.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/h&m/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/barnes&noble/expected-title.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/barnes&noble/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/bose/expected-title.html', 'utf-8')),
+     fs.readFileSync(__dirname + '/../test/product_classification_test_data/bose/source.html', 'utf-8')
+    ],
+    [staticDom(fs.readFileSync(__dirname + '/../test/product_classification_test_data/target/expected-title.html', 'utf-8')),
+      fs.readFileSync(__dirname + '/../test/product_classification_test_data/target/source.html', 'utf-8')
+    ]
+  ];
+}
+
 
 if (require.main === module) {
-    let coeffs = [1.5, 4.5, 2, 6.5, 2, 0.5, 0];
-    console.log('Tuned coefficients:', coeffs);
-    console.log('% difference from ideal:',
-      deviationScore(productDocPairs(), coeffs));
+    window.width = 1661;
+    window.height = 562;
+    console.log('% difference from ideal:', deviationScore(productImageDocPairs(), 'image'));
+    console.log('% difference from ideal:', deviationScore(productTitleDocPairs(), 'title'));
 
 }
 
 module.exports = {
     deviationScore,
-    productDocPairs,
+    productImageDocPairs,
+    productTitleDocPairs,
     tunedImageFnodes
 };
--- test/product-test.js
+++ test/product-test.js
@@ -1,26 +1,14 @@
 const assert = require('chai').assert;
+const {deviationScore, productImageDocPairs, productTitleDocPairs} = require('../examples/products');
 
-const {deviationScore, productDocPairs, tunedContentFnodes} = require('../examples/products');
-const {staticDom} = require('../utils');
 
 
 describe('Products ruleset finds content from...', function () {
     this.timeout(0);  // This early in the dev process, some things still take awhile.
-
-    // ---------------------- Test helper routines: -----------------------
-
-    /**
-     * Return a 20-char snippet of each node from a document's main
-     * textual content.
-     */
-    function snippetsFrom(doc) {
-        return tunedContentFnodes()(doc).map(fnode => fnode.element.textContent.trim().substr(0, 20).trim());
-    }
-
-
     // ----------------------------- Tests: -------------------------------
 
     it('the Products test suite', function () {
-        assert.isBelow(deviationScore(productDocPairs()), 100);
+        assert.isBelow(deviationScore(productImageDocPairs(), 'image'), 100);
+        assert.isBelow(deviationScore(productTitleDocPairs(), 'title'), 100);
     });
 });
