11916f842715da2d965fe6259691cee879b0fbfe
Erik Rose <erik@mozilla.com>
2019-12-30 12:50:26 -0500

    Support multiple features graphed at once.

--- cli/fathom_web/commands/histogram.py
+++ cli/fathom_web/commands/histogram.py
@@ -1,24 +1,50 @@
 from json import load
 
-from click import argument, command, File, option, progressbar
-from plotly.express import histogram
+from click import argument, command, File, option
+from plotly.graph_objects import Histogram
 from plotly.subplots import make_subplots
 
-from ..utils import classifier, tensor, tensors_from
+from ..utils import tensors_from
 
 
 @command()
 @argument('vector_file', type=File('r'))
-@argument('feature', type=str)
+@option('feature', '-f',
+        type=str,
+        default='',
+        help='The name of the feature to graph. Omit to graph all features.')
 def main(vector_file, feature):
-    """Print a histogram of the given feature."""
+    """Print a histogram of one or more features."""
     data = load(vector_file)
     x, y, num_yes = tensors_from(data['pages'])
+    feature_names = data['header']['featureNames']
+    if feature:
+        feature_index = feature_names.index(feature)
+        features = [values_for_feature(data['pages'], feature_index)]
+    else:
+        features = [values_for_feature(data['pages'], feature_index)
+                    for feature_index in range(len(feature_names))]
 
-    feature_index = data['header']['featureNames'].index(feature)
-    feature_values = []
-    for page in data['pages']:
-        for node in page['nodes']:
-            feature_values.append(node['features'][feature_index])
+    COLS = 4
+    rows = len(features) // COLS + (1 if len(features) % COLS else 0)
+    figure = make_subplots(
+        rows=rows,
+        cols=min(COLS, len(features)),
+        subplot_titles=feature_names[:len(features)]
+    )
+    row = col = 1
+    for f in features:
+        figure.add_trace(Histogram(name=feature, x=f, nbinsx=40),
+                         row=row,
+                         col=col)
+        col += 1
+        if col > COLS:
+            col = 1
+            row += 1
+    figure.show()
 
-    histogram({feature: feature_values}, x=feature, nbins=40).show()
+
+def values_for_feature(pages, feature_index):
+    return [node['features'][feature_index]
+            for page in pages
+            for node in page['nodes']]
